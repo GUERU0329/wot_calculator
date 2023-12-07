f4dc56a34272e501252571b6ad893f0367d90c14
GUERU <ryuta12356@gmail.com>
2023-12-06 17:55:18 -0800

    車長用視認性向上装置の追加

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { useState, useEffect  } from 'react';
-import MapComponent from './MapComponent'; // MapComponentのパスを正しく指定してください
+import MapComponent from './MapComponent'; 
 import './App.css';
 
-// Control Panel for input values
-function ControlPanel({ onInputChange }) {
+function ControlPanel({ onInputChange, onEquipmentChange, onMovementChange  }) {
   return (
-    <div>
-      <input className="input-field" type="number" placeholder="自車両の視界範囲" onChange={(e) => onInputChange('viewRange', e.target.value)} />
-      <input className="input-field" type="number" placeholder="敵車両の隠蔽率(%)" onChange={(e) => onInputChange('concealment', e.target.value)} />
-      <input className="input-field" type="number" placeholder="茂みの数" onChange={(e) => onInputChange('bushCount', e.target.value)} />
+<div>
+  <input className="input-field" type="number" placeholder="自車両の視界範囲" onChange={(e) => onInputChange('viewRange', e.target.value)} />
+  <input className="input-field" type="number" placeholder="敵車両の隠蔽率(%)" onChange={(e) => onInputChange('concealment', e.target.value)} />
+  <select className="input-field" onChange={(e) => onInputChange('bushCount', e.target.value)}>
+        <option value="0">草むら隠蔽効果</option>
+        <option value="1">25%</option>
+        <option value="2">50%</option>
+        <option value="3">75%</option>
+        <option value="4">80%</option>
+        </select>
+  <div className="checkbox-container">
+    <input
+      type="checkbox"
+      id="movementMoving"
+      onChange={(e) => onMovementChange(e.target.checked)}
+    />
+    <label htmlFor="movementMoving">敵車両移動中　</label>
+  <select className="select-field select-field-equipment" onChange={(e) => onEquipmentChange('commanderEquipmentType', e.target.value)}>
+          <option value="none">車長用視認性向上装置</option>
+          <option value="10-15">移動中隠蔽率10%減少/草むら隠蔽ボーナス15%減少</option>
+          <option value="12.5-20">移動中隠蔽率12.5%移動中/草むら隠蔽ボーナス20%減少</option>
+        </select>
     </div>
+    </div>  
   );
 }
 
@@ -104,6 +122,7 @@ const mapsConfig = [
 
 
 function App() {
+  const [enemyMovement, setEnemyMovement] = useState('stationary');
   const [viewRange, setViewRange] = useState(0);
   const [concealment, setConcealment] = useState(0);
   const [bushCount, setBushCount] = useState(0);
@@ -113,21 +132,50 @@ function App() {
 
   const handleInputChange = (field, value) => {
     if (field === 'viewRange') setViewRange(value);
-    if (field === 'concealment') setConcealment(value / 100); // percentage to decimal
+    if (field === 'concealment') setConcealment(value / 100); 
     if (field === 'bushCount') setBushCount(value);
   };
+  const [commanderEquipmentType, setCommanderEquipmentType] = useState('none');
+// handleEquipmentChange 関数の外に handleMovementChange 関数を移動
+const handleMovementChange = (isMoving) => {
+  setEnemyMovement(isMoving ? 'moving' : 'stationary');
+};
+
+// handleEquipmentChange 関数
+const handleEquipmentChange = (field, value) => {
+  if (field === 'commanderEquipmentType') setCommanderEquipmentType(value);
+};
+  
 
   useEffect(() => {
-    let bushEffect = bushCount * 0.25;
-    bushEffect = bushEffect > 0.8 ? 0.8 : bushEffect; // Max bush effect capped at 80%
-    const totalConcealment = concealment + bushEffect;
+    const equipmentEffect = commanderEquipmentType === '10-15' ? 0.1 : (commanderEquipmentType === '12.5-20' ? 0.125 : 0);
+    const bushEffectReduction = commanderEquipmentType === '10-15' ? 0.15 : (commanderEquipmentType === '12.5-20' ? 0.20 : 0);
+  
+    let effectiveConcealment = concealment;
+    if (commanderEquipmentType !== 'none' && enemyMovement === 'moving') {
+      effectiveConcealment -= concealment * equipmentEffect;
+    }
+    effectiveConcealment = Math.max(0, effectiveConcealment);
+  
+    let bushEffect = bushCount * 0.25 - bushCount * 0.25 * bushEffectReduction;
+    bushEffect = Math.max(0, bushEffect);
+    bushEffect = bushEffect > 0.8 ? 0.8 : bushEffect;
+  
+    const totalConcealment = effectiveConcealment + bushEffect;
     let calculatedDetectionDistance = viewRange - ((viewRange - 50) * totalConcealment);
-
-    // Ensure that the detection distance does not exceed 455 meters
-    calculatedDetectionDistance = calculatedDetectionDistance > 455 ? 455 : calculatedDetectionDistance;
+  
+    if (viewRange <= 50 || viewRange === 0) {
+      calculatedDetectionDistance = Math.max(0, calculatedDetectionDistance);
+    } else {
+      calculatedDetectionDistance = Math.max(50, calculatedDetectionDistance);
+    }
+  
+    calculatedDetectionDistance = Math.min(445, calculatedDetectionDistance);
     
     setDetectionDistance(calculatedDetectionDistance);
-  }, [viewRange, concealment, bushCount]);
+  }, [viewRange, concealment, bushCount, commanderEquipmentType, enemyMovement]);
+  
+  
 
 
   return (
@@ -135,7 +183,13 @@ function App() {
       <div className='text'>
         WoT発見距離計算機
       </div>
-      <ControlPanel className="panel" onInputChange={handleInputChange} />
+      <ControlPanel 
+  className="panel" 
+  onInputChange={handleInputChange} 
+  onEquipmentChange={handleEquipmentChange} 
+  onMovementChange={handleMovementChange} // この行を確認
+/>
+
       <div className='text'>
         発見距離: {detectionDistance}m
       </div>
